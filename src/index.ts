a321760f4924407548717cc729e8de3dc7167f4a
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2022-10-19 11:26:57 +0900

    ✅ prismaのセットアップ

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,6 @@
-import { createServer } from "@graphql-yoga/node";
-import fastify, { FastifyRequest, FastifyReply } from "fastify";
+import dotenv from "dotenv";
+dotenv.config();
 
-// This is the fastify instance you have created
-const app = fastify({ logger: true });
+import { startServer } from "./server";
 
-const graphQLServer = createServer<{
-  req: FastifyRequest;
-  reply: FastifyReply;
-}>({
-  // Integrate Fastify logger
-  logging: {
-    debug: (...args) => args.forEach((arg) => app.log.debug(arg)),
-    info: (...args) => args.forEach((arg) => app.log.info(arg)),
-    warn: (...args) => args.forEach((arg) => app.log.warn(arg)),
-    error: (...args) => args.forEach((arg) => app.log.error(arg)),
-  },
-});
-
-/**
- * We pass the incoming HTTP request to GraphQL Yoga
- * and handle the response using Fastify's `reply` API
- * Learn more about `reply` https://www.fastify.io/docs/latest/Reply/
- **/
-app.route({
-  url: "/graphql",
-  method: ["GET", "POST", "OPTIONS"],
-  async handler(req, reply) {
-    // Second parameter adds Fastify's `req` and `reply` to the GraphQL Context
-    const response = await graphQLServer.handleIncomingMessage(req, {
-      req,
-      reply,
-    });
-    response.headers.forEach((value, key) => {
-      reply.header(key, value);
-    });
-
-    reply.status(response.status);
-
-    reply.send(response.body);
-
-    return reply;
-  },
-});
-
-app.listen(4000);
+startServer();
---
+++ src/server.ts
@@ -0,0 +1,62 @@
+import fastify, {
+  FastifyInstance,
+  FastifyReply,
+  FastifyRequest,
+  FastifyServerOptions,
+} from "fastify";
+
+import { createServer } from "@graphql-yoga/node";
+import { schema } from "./schema";
+
+import prismaPlugin from "./plugins/prisma";
+import { Context } from "./context";
+import statusPlugin from "./plugins/status";
+
+const app = fastify({ logger: true });
+
+const graphQLServer = createServer<{
+  req: FastifyRequest;
+  reply: FastifyReply;
+}>({
+  // Integrate Fastify logger
+  logging: {
+    debug: (...args) => args.forEach((arg) => app.log.debug(arg)),
+    info: (...args) => args.forEach((arg) => app.log.info(arg)),
+    warn: (...args) => args.forEach((arg) => app.log.warn(arg)),
+    error: (...args) => args.forEach((arg) => app.log.error(arg)),
+  },
+});
+
+app.register(statusPlugin);
+app.register(prismaPlugin);
+
+app.route({
+  url: "/graphql",
+  method: ["GET", "POST", "OPTIONS"],
+  async handler(req, reply) {
+    // Second parameter adds Fastify's `req` and `reply` to the GraphQL Context
+    const response = await graphQLServer.handleIncomingMessage(req, {
+      req,
+      reply,
+    });
+    response.headers.forEach((value, key) => {
+      reply.header(key, value);
+    });
+
+    reply.status(response.status);
+
+    reply.send(response.body);
+
+    return reply;
+  },
+});
+
+export async function startServer() {
+  try {
+    const port = process.env.PORT ?? 4000;
+    await app.listen(port, "0.0.0.0");
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+}
